4975ca5ee6ae371e938f773ae228d464717bed9b
Lucas Augusto <lucas.tansini@hotmail.com>
2018-04-05 09:17:19 -0300

    Updated set_mentor.sh
    Now the set_mentor has a modular approach to user changing.

--- rvex-release-4.2/platform/cache-test/settings64.sh
+++ rvex-release-4.2/platform/cache-test/settings64.sh
@@ -2,6 +2,6 @@
 # Copyright (c) 1986-2018 Xilinx, Inc.  All rights reserved. #
 ##############################################################
 
-source /home/tansini/Xilinx/Vivado_HLS/2015.4/.settings64-Vivado_High_Level_Synthesis.sh
-source /home/tansini/Xilinx/Vivado/2015.4/.settings64-Vivado.sh
-source /home/tansini/Xilinx/DocNav/.settings64-DocNav.sh
+source /home/latansini/Vivado_HLS/2015.4/.settings64-Vivado_High_Level_Synthesis.sh
+source /home/latansini/Vivado/2015.4/.settings64-Vivado.sh
+source /home/latansini/DocNav/.settings64-DocNav.sh
