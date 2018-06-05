1f0504e320cb3688d59e9800f38f6297aadb13bb
Lucas Augusto <lucas.tansini@hotmail.com>
2018-06-05 11:20:32 -0300

    Added SIMBAH-FI files
    SIMBAH-FI files are added, along with a set_mentor.sh needed to execute the first script: 'run-fault-injection.sh'. Note that SIMBAH is not working atm.

---
+++ set_mentor.sh
@@ -0,0 +1,5 @@
+user="latansini" 
+export PATH=$PATH:/home/$user/modelsim/modeltech/linux_x86_64/
+export LM_LICENSE_FILE=/home/$user/license.dat 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/$user/modelsim/modeltech/lib32/ 
+export LD_PRELOAD=/home/$user/modelsim/modeltech/lib32/libfreetype.so 
